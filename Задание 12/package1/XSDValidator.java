3a03b3a2ca9e1fb61c7e5464f1474ac45e406a48
0MG-DEN <loooool2312@gmail.com>
2018-12-24 15:06:47 +0300

    Задание 12

--- Задание 12/package1/XSDValidator.java
+++ Задание 12/package1/XSDValidator.java
@@ -13,14 +13,13 @@ public class XSDValidator
     public static void validateXMLSchema(String xsdPath, String xmlPath) throws SAXException
     {
         try {
-            // Получить фабрику для схемы
+			
             SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            // Загрузить схему из XSD
+			
             Schema schema = factory.newSchema(new File(xsdPath));
-            // Создать валидатор (проверялбщик)
+			
             Validator validator = schema.newValidator();
-            // Запусить проверку - если будет исключение, значит есть ошибки.
-            // Если нет - все заполнено правильно
+			
             validator.validate(new StreamSource(new File(xmlPath)));
         } catch (IOException e) {
             System.out.println("Exception: " + e.getMessage());
